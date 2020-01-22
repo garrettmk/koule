07b65a50d97ec8361882a8d3fad33db2eea02322
Garrett Myrick <garrett@squarepractice.com>
2020-01-22 09:07:49 -0800

    Updated clip-path and Divider styling

--- src/components/Divider/index.js
+++ src/components/Divider/index.js
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 export const Divider = styled.hr`
   color: ${ props => props.theme.colors.divider };
+  background-color: ${ props => props.theme.colors.divider };
 `;
