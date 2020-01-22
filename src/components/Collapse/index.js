2f8e7a21f7af9bc10a864b200370836bd242b77b
Garrett Myrick <garrett@squarepractice.com>
2020-01-22 08:11:09 -0800

    Added ResizeObserver polyfill

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSpring } from "react-spring";
+import { ResizeObserver } from '@juggle/resize-observer';
 import useMeasure from 'react-use-measure';
 import * as S from './styled';
 
 function _Outer({ open = true, style, children, ...props }) {
-  const [ref, bounds] = useMeasure();
+  const [ref, bounds] = useMeasure({ polyfill: ResizeObserver });
   const animatedProps = useSpring({ height: open ? bounds.height : 0 });
   const innerElement = React.Children.only(children);
   const innerElWithRef = React.cloneElement(innerElement, { ref });
